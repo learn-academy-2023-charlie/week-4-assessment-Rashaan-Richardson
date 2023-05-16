a1f7929ace82681d4602c446a4f9ca21655a3d9b
Rashaan-Richardson <rashaan.richardson@gmail.com>
2023-05-16 08:22:45 -0600

    week 4 work

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,7 +13,17 @@
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
 // a) Create a test with an expect statement using the variable provided.
-// HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+// describe("remove_color", () => {
+//     it("Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+//         const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+//         // Expected output: ["yellow", "blue", "pink", "green"]
+//         const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+//         // Expected output: [["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]   
+//         expect(remove_color(colors1)).toEqual(["yellow", "blue", "pink", "green"])
+//         expect(remove_color(colors2)).toEqual(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"])
+//     })
+// })
+// HINT: Check out this resource:  
 
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
@@ -27,11 +37,32 @@ const colors2 = [
 ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+// Pseudo code:
+// input: array
+// output : array without 0 index
+
 // b) Create the function that makes the test pass.
+const remove_color = (element) => {
+  // use a method that can filter each element in the array. Then stack that with a indexing method such as bracket notation or slice. Just need to get rid of the 0 index 
+  return element.filter((value) => value).slice(1,element.length)
+}
+console.log(remove_color(colors1));
+console.log(remove_color(colors2));
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
+// describe(votes", () => {
+//     it("Create a function that takes in an object that contains up votes and down votes and returns the end tally", () => {
+//         const votes1 = { upVotes: 13, downVotes: 2 }
+//         Expected output: 11
+//         const votes2 = { upVotes: 2, downVotes: 33 }
+//          Expected output: -31   
+//         expect(votes(votes1)).toEqual(11)
+//         expect(votes(votes2)).toEqual(-31)
+//     })
+// })
 
 const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
@@ -40,12 +71,51 @@ const votes2 = { upVotes: 2, downVotes: 33 }
 
 // b) Create the function that makes the test pass.
 
+// Pseudo code:
+// input: object
+// output : sum of the values in the object
+
+const votes = (element) =>{
+  // ideally it should be straight forward. Subtract the key upvotes from the key downvotes
+  return element['upVotes'] - element['downVotes']
+
+}
+
+console.log(votes(votes1));
+console.log(votes(votes2));
+
+
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+// describe("unique_arr", () => {
+//     it("Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.", () => {
+//          const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
+//          const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
+//          Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+//          expect(unique_arr = (element1,element2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+//     })
+// })
 
 const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
 // b) Create the function that makes the test pass.
+// Pseudo code:
+// input: multiple arrays
+// output : a single array with no duplicates
+
+
+const unique_arr = (element1,element2) => {
+  // Create a variable that stores a concated array of all the parameters. Then use set notation to remove the duplicates. 
+  new_arr = element1.concat(element2)
+  return new Set(new_arr)
+}
+
+const unique_arr1 = ([...element]) => {
+    return new Set(element.concat([...element]))
+  }
+
+console.log(unique_arr(dataTypesArray1,dataTypesArray2));
+console.log(unique_arr(dataTypesArray1,dataTypesArray2));
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,17 @@ reposts2 = 42
 reposts3 = 221
 # Expected output: '221 is odd'
 
+def divy (element)
+    if element.remainder(2) == 0
+        "#{element} is even"
+    else 
+        "#{element} is odd"
+    end 
+end
+
+p divy(reposts1)
+p divy(reposts2)
+p divy(reposts3)
 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
@@ -22,6 +33,13 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def remove_vowels(element)
+    element.delete('aeiou')
+end
+
+p remove_vowels(beatles_album1)
+p remove_vowels(beatles_album2)
+p remove_vowels(beatles_album3)
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
@@ -32,3 +50,15 @@ palindrome_test_case2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_test_case3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def pally(element)
+    if element.downcase.reverse == element.downcase
+        "#{element} is a palindrome"
+    else
+        "#{element} is not a palindrome"
+    end 
+end
+
+p pally(palindrome_test_case1)
+p pally(palindrome_test_case2)
+p pally(palindrome_test_case3)
